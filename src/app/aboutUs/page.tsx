2772f3d677a10b2dd7076f8b61f329b29f43037e
HarshDhaduk2001 <harshdhaduk2001@gmail.com>
2023-09-12 21:16:10 +0530

    about page ui completed

--- src/app/aboutUs/page.tsx
+++ src/app/aboutUs/page.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+"use client";
 import React from "react";
 import {
   AddCart,
@@ -40,10 +41,17 @@ const mission = [
 ];
 
 const page = () => {
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+  };
   return (
     <>
       <div className="relative text-right">
-        <img src={Hero.src} alt="dog" className="w-full h-[300px] lg:h-[700px]" />
+        <img
+          src={Hero.src}
+          alt="dog"
+          className="w-full h-[300px] lg:h-[700px]"
+        />
         <span className="absolute top-24 right-5">
           <p className="text-white font-bold text-[28px] lg:hidden">
             Thai Awesome Crafted by Heart, Loved by your pets
@@ -66,7 +74,9 @@ const page = () => {
         </span>
       </div>
       <div className="flex flex-col items-center justify-center my-7">
-        <span className="text-4xl font-bold mb-2 lg:mb-0">Our Value</span>
+        <span className="text-3xl lg:text-4xl font-bold mb-2 lg:mb-0">
+          Our Value
+        </span>
         <div className="flex flex-col lg:flex-row items-center justify-center gap-5 lg:gap-20 w-[100%] py-5 lg:py-10">
           <div className="flex flex-col items-center w-[80%] lg:w-[20%] text-sm lg:text-base gap-4">
             <span className="relative">
@@ -137,14 +147,14 @@ const page = () => {
             <br /> to society, communities and environment.
           </span>
           <span className="lg:hidden">
-            Our vision is to be recognized by international standards as world 
-            class premium innovation pet food producer and exporter with 
+            Our vision is to be recognized by international standards as world
+            class premium innovation pet food producer and exporter with
             responsibility to society, communities and environment.
           </span>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center my-7 lg:mt-24 lg:mb-32">
-        <span className="text-4xl font-bold">Mission</span>
+        <span className="text-3xl lg:text-4xl font-bold">Mission</span>
         <div className="flex flex-col lg:flex-row items-center justify-center gap-5 lg:gap-20 w-[100%] py-5 lg:py-10">
           {mission.map((i: any, index: number) => (
             <div
@@ -161,6 +171,23 @@ const page = () => {
             </div>
           ))}
         </div>
+      </div>
+      <div className="relative hidden lg:block">
+        <span className="absolute -bottom-20 -z-10">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="535"
+            height="489"
+            viewBox="0 0 535 489"
+            fill="none"
+          >
+            <path
+              opacity="0.15"
+              d="M61 169C33 81 -14.6667 10.5 -50.5 0.5L-59 489H512C512.667 479.833 570.5 444 498.5 395C426.5 346 377.5 314 258 308C138.5 302 89 257 61 169Z"
+              fill="#FC8321"
+            />
+          </svg>
+        </span>
       </div>
       <div className="flex items-center justify-center py-12 my-2 lg:my-10 bg-blue lg:w-[100%]">
         <div className="text-white gap-2 flex flex-col justify-center lg:text-left lg:w-[50%] px-8 lg:pl-16">
@@ -184,6 +211,183 @@ const page = () => {
             className="w-[500px] h-[500px] absolute left-0 -bottom-60 "
           />
         </div>
+      </div>
+      <div className="relative hidden lg:block">
+        <span className="absolute -top-28 -z-10">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="707"
+            height="445"
+            viewBox="0 0 707 445"
+            fill="none"
+          >
+            <path
+              opacity="0.15"
+              d="M85 394.62C33 461.42 -98.6667 447.12 -158 431.62L-150 25.6199C144.167 6.28656 727.1 -20.7801 705.5 25.6199C678.5 83.6199 541 126.62 323.5 161.12C106 195.62 150 311.12 85 394.62Z"
+              fill="#FC8321"
+            />
+          </svg>
+        </span>
+      </div>
+      <div className="flex flex-col items-center justify-center mt-7 lg:mt-24">
+        <span className="text-3xl lg:text-4xl font-bold">
+          Message from Founders
+        </span>
+        <div className=" items-center justify-center gap-5 lg:gap-20 w-[100%] py-5 lg:py-10 lg:px-52">
+          <div className="flex flex-col lg:flex-row items-center justify-center w-[100%]">
+            <span className="w-[80%] lg:w-[50%] lg:pr-8">
+              <p>
+                Woffy has its humble beginnings from a very personal story; as
+                dog lovers and pet parents of the world&apos;s best dog (yes,
+                every pet parent says this). Rex came into our lives and changed
+                it forever. For us, Rex is a companion that always loves, a
+                friend that never judges and a child that is always ready to
+                have some fun. He is truly the heart and soul of our family.
+              </p>
+              <p className="lg:mt-8">
+                As pet parents, we totally understand the feeling of celebrating
+                life with your pet. Somethings just can’t be explained; be it
+                the excitement of shopping for your pet, the comfort of a cuddle
+                or the workday guilt. We know the pet-parenting journey is full
+                of up’s and down’s, but what if you had someone to hold your
+                hand and support your through this journey? Rex ignited a
+                passion so strong that we saw ourselves travelling from
+                Ahmedabad to Pune to share our love for pets with India. Also we
+                have our own plant at Pune area with products making in
+                machineries and will be all the Made In India product.
+              </p>
+            </span>
+            <span className="w-[50%] mt-8 lg:mt-0">
+              <img
+                src={Family.src}
+                alt="family"
+                className="lg:w-[500px] lg:h-[400px]"
+              />
+            </span>
+          </div>
+          <p className="mx-10 flex items-center justify-center my-5">
+            For every products and process of Woffy Pet Shop, we always consider
+            and focus on customers&apos; safety and satisfaction. Additionally,
+            we also focus on environmental safety along with the highest quality
+            standard. We operate our business with care of transparency and good
+            governance so that there ​will be a consistency in social
+            development in all sectors and in the country.
+          </p>
+          <p className="mx-10 flex items-center justify-center">
+            Above all, we intend to develop our products to be recognized by
+            international standards as well as upholding the responsibility to
+            return good deeds to our country and people. We help to develop and
+            strengthen the value of the quality of life for employees, partners
+            and local communities. This is for the better living, well-being,
+            and sustainability.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center mt-7 lg:mt-24 bg-[#FEF0F0]">
+        <span className="text-3xl lg:text-4xl font-bold mt-8">
+          Local Matters
+        </span>
+        <div className="flex items-center justify-center gap-5 lg:gap-20 w-[100%] py-5 lg:py-10 lg:px-52">
+          <span className="w-[80%] lg:w-[70%] lg:pr-8">
+            <p>
+              We believe in supporting our local community. For us, it is about
+              nurturing local businesses so that we can grow together, and in
+              turn provide the best products and services to enrich the lives of
+              pets.
+            </p>
+            <p className="my-2">
+              That is why at Woffy, a large proportion of our products are
+              sourced from independent curators based in India. We made products
+              in India and have all the manufacturing units in the India base
+              only. Also we have all the products certified and licenses from
+              the respected agencies and other government bodies.
+            </p>
+            <p>
+              Our food product is all certified from FSSAI, our products like
+              harness and pet belt are also certified from the ISI unit of
+              government safety bodies. We use original brand to make our
+              products and the procedure of the products are mandatory to be
+              very proper and made with all safety and security. Our Packing
+              products are being done in India and are totally done by the
+              machinery based packings. We are proud of our country and would
+              like to be the part of Made in India forum.
+            </p>
+          </span>
+          <span className="w-[30%] hidden lg:block">
+            <img
+              src={Indian.src}
+              alt="indian"
+              className="w-[500px] h-[400px]"
+            />
+          </span>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center my-7 gap-3 lg:gap-8">
+        <span className="text-[14px] lg:text-[24px] mt-5 flex items-center justify-center text-center mx-8">
+          Once ordered, your products will be home-delivered as per the order
+          details.
+        </span>
+        <span className="text-[14px] lg:text-[24px] flex items-center justify-center mx-8">
+          We also offer our services globally.
+        </span>
+        <span className="text-[14px] lg:text-[24px] mb-5 flex items-center justify-center mx-8">
+          Write us for feedback or suggestion
+        </span>
+        <form
+          className="flex flex-col items-center justify-center gap-8"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex flex-col lg:flex-row items-center justify-center gap-8">
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter Name"
+              className="border border-gray-500 py-2 px-4 w-[120%] lg:w-[50%]"
+            />
+            <input
+              type="tel"
+              name="tel"
+              placeholder="Enter Phone No."
+              className="border border-gray-500 py-2 px-4 w-[120%] lg:w-[50%]"
+            />
+          </div>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter Email"
+            className="border border-gray-500 py-2 px-4 w-[100%]"
+          />
+          <textarea
+            name="comment"
+            cols={30}
+            rows={4}
+            placeholder="Enter Comment"
+            className="border border-gray-500 py-2 px-4 w-[100%]"
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-orange text-white py-2 px-4 rounded-lg w-[100%]"
+          >
+            Send
+          </button>
+        </form>
+      </div>
+      <div className="relative hidden lg:block">
+        <span className="absolute -bottom-8 right-0 -z-10">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="683"
+            height="445"
+            viewBox="0 0 683 445"
+            fill="none"
+          >
+            <path
+              opacity="0.15"
+              d="M621.084 49.9787C673.084 -16.8213 804.751 -2.52127 864.084 12.9787L856.084 418.979C561.917 438.312 -21.016 465.379 0.583984 418.979C27.584 360.979 165.084 317.979 382.584 283.479C600.084 248.979 556.084 133.479 621.084 49.9787Z"
+              fill="#FC8321"
+            />
+          </svg>
+        </span>
       </div>
     </>
   );
